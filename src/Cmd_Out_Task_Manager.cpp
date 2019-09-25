408f76248c617fc58f84ae910776de704288e5e5
Jaume Bosch <jbosch@bsc.es>
2019-09-25 11:43:42 +0100

    Update LICENSE to GNU LGPL v3

--- src/Cmd_Out_Task_Manager.cpp
+++ src/Cmd_Out_Task_Manager.cpp
@@ -1,22 +1,22 @@
-/*------------------------------------------------------------------------*/
-/*    (C) Copyright 2017-2019 Barcelona Supercomputing Center             */
-/*                            Centro Nacional de Supercomputacion         */
-/*                                                                        */
-/*    This file is part of OmpSs@FPGA toolchain.                          */
-/*                                                                        */
-/*    This code is free software; you can redistribute it and/or modify   */
-/*    it under the terms of the GNU General Public License as published   */
-/*    by the Free Software Foundation; either version 3 of the License,   */
-/*    or (at your option) any later version.                              */
-/*                                                                        */
-/*    OmpSs@FPGA toolchain is distributed in the hope that it will be     */
-/*    useful, but WITHOUT ANY WARRANTY; without even the implied          */
-/*    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    */
-/*    See the GNU General Public License for more details.                */
-/*                                                                        */
-/*    You should have received a copy of the GNU General Public License   */
-/*    along with this code. If not, see <www.gnu.org/licenses/>.          */
-/*------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------
+  (C) Copyright 2017-2019 Barcelona Supercomputing Center
+                          Centro Nacional de Supercomputacion
+
+  This file is part of OmpSs@FPGA toolchain.
+
+  This code is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 3 of
+  the License, or (at your option) any later version.
+
+  OmpSs@FPGA toolchain is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the implied
+  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this code. If not, see <www.gnu.org/licenses/>.
+--------------------------------------------------------------------*/
 
 #include <ap_axi_sdata.h>
 #include <hls_stream.h>
