5b98f41b3dd25cc9b273a98d59dfcd14f8276773
Juan Miguel de Haro <juan.deharoruiz@bsc.es>
2020-04-01 15:50:16 +0200

    Check synthesis errors

--- scripts/synthesize_som.tcl
+++ scripts/synthesize_som.tcl
@@ -29,3 +29,8 @@ launch_runs synth_1
 
 wait_on_run synth_1
 
+# Check if synthesis finished correctly
+if {[string match "*ERROR*" [get_property STATUS [get_runs synth_1]]]} {
+	error "ERROR: Hardware synthesis failed."
+}
+
